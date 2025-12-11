9ae0572a6ddd944fba985a5e59d82355e967d009
OmChaitanyaSharma <omranchi732@gmail.com>
2025-12-11 16:46:52 +0530

    commit 1

---
+++ LAB2_1.java
@@ -0,0 +1,8 @@
+public class LAB2_1{
+    public static void main(String[] args) {
+        System.out.println("NAME - Om Chaitanya Sharma");
+        System.out.println("Class - CSE48");
+        System.out.println("ROLL - 24155556");
+        System.out.println("BRANCH - CSE(AI&ML");
+    }
+}
